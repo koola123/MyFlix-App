e78e373c8f468bbded338d36b03ec60ad41eb837
koola123 <82808273+koola123@users.noreply.github.com>
2021-08-24 00:54:55 +0200

    added actions.js, reducers.js, additonal views in connection with Redux

---
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -0,0 +1,18 @@
+// src/components/visibility-filter-input/visibility-filter-input
+import React from 'react';
+import { connect } from 'react-redux';
+import Form from 'react-bootstrap/Form';
+import { setFilter } from '../../actions/actions';
+
+function VisibilityFilterInput(props) {
+  return <Form.Control
+    onChange={e => props.setFilter(e.target.value)}
+    value={props.visibilityFilter}
+    placeholder="filter movies"
+  />;
+}
+
+export default connect(
+  null,
+  { setFilter }
+)(VisibilityFilterInput);
