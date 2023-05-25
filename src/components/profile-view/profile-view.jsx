3b7435df064c336b7a41765dd8f0acca6add7277
Matthias Müringer <mmueringer@web.de>
2023-05-25 20:17:05 +0200

    fix: show favorite movies in profile view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -35,9 +35,9 @@ export class ProfileView extends React.Component {
       .then((response) => {
         console.log(response);
         this.setState({
-          Username: response.data.Username, 
+          Username: response.data.Username,
           Email: response.data.Email,
-          Birthday: response.data.Birthday.substring(0,10),
+          Birthday: response.data.Birthday.substring(0, 10),
           FavoriteMovies: response.data.FavoriteMovies,
         });
       })
@@ -93,7 +93,7 @@ export class ProfileView extends React.Component {
           Username: this.state.Username,
           Password: this.state.Password,
           Email: this.state.Email,
-          Birthday: this.state.Birthday.substring(0,10)
+          Birthday: this.state.Birthday.substring(0, 10),
         },
         {
           headers: { Authorization: `Bearer ${token}` },
@@ -138,19 +138,19 @@ export class ProfileView extends React.Component {
   setPassword(value) {
     this.setState({
       Password: value,
-    })
+    });
   }
 
   setEmail(value) {
     this.setState({
       Email: value,
-    })
+    });
   }
 
   setBirthday(value) {
     this.setState({
       Birthday: value,
-    })
+    });
   }
 
   render() {
@@ -161,101 +161,103 @@ export class ProfileView extends React.Component {
         {/* Profile View */}
         <Card className="profile-card">
           <h1 className="profile-title">Profile View</h1>
-            <Form
-              noValidate
-              validated={validated}
-              className="update-form"
-              onSubmit={(e) =>
-                this.handleUpdate(e,
-                  this.state.Username,
-                  this.state.Password,
-                  this.state.Email,
-                  this.state.Birthday
-                )
-              }
+          <Form
+            noValidate
+            validated={validated}
+            className="update-form"
+            onSubmit={(e) =>
+              this.handleUpdate(
+                e,
+                this.state.Username,
+                this.state.Password,
+                this.state.Email,
+                this.state.Birthday
+              )
+            }
+          >
+            <Form.Group controlId="Username">
+              <Form.Control
+                type="text"
+                size="md"
+                placeholder="Username"
+                value={this.state.Username}
+                required
+                onChange={(e) => this.setUsername(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="Password">
+              <Form.Control
+                type="password"
+                size="md"
+                placeholder="Password"
+                value={this.state.Password}
+                required
+                onChange={(e) => this.setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="Email">
+              <Form.Control
+                type="email"
+                size="md"
+                placeholder="Email"
+                value={this.state.Email}
+                required
+                onChange={(e) => this.setEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="Birthday">
+              <Form.Control
+                type="date"
+                size="md"
+                value={this.state.Birthday}
+                onChange={(e) => this.setBirthday(e.target.value)}
+              />
+            </Form.Group>
+            <Button id="btn-update-my-account" variant="primary" type="submit">
+              Update Profile
+            </Button>
+            <Button
+              id="btn-delete-my-account"
+              variant="secondary"
+              onClick={(e) => this.handleDeleteUser(e)}
             >
-              <Form.Group controlId="Username">
-                <Form.Control
-                  type="text"
-                  size="md"
-                  placeholder="Username"
-                  value={this.state.Username}
-                  required
-                  onChange={(e) => this.setUsername(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId="Password">
-                <Form.Control
-                  type="password"
-                  size="md"
-                  placeholder="Password"
-                  value={this.state.Password}
-                  required
-                  onChange={(e) => this.setPassword(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId="Email">
-                <Form.Control
-                  type="email"
-                  size="md"
-                  placeholder="Email"
-                  value={this.state.Email}
-                  required
-                  onChange={(e) => this.setEmail(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId="Birthday">
-                <Form.Control
-                  type="date"
-                  size="md"
-                  value={this.state.Birthday}
-                  onChange={(e) => this.setBirthday(e.target.value)}
-                />
-              </Form.Group>
-                <Button 
-                id="btn-update-my-account" 
-                variant="primary"
-                type="submit">
-                  Update Profile
-                </Button>
-                <Button
-                  id="btn-delete-my-account"
-                  variant="secondary"
-                  onClick={(e) => this.handleDeleteUser(e)}
-                >
-                  Delete Account
-                </Button>
-            </Form>
+              Delete Account
+            </Button>
+          </Form>
           {/* Favorite Movies */}
           <React.Fragment>
-          <h1 className="favorite-movies-title">Favorite Movies:</h1>
+            <h1 className="favorite-movies-title">Favorite Movies:</h1>
             <Card className="d-flex favorites-movies ">
               {FavoriteMovies.length > -1 &&
                 FavoriteMovies.map((movieId) => {
                   const movie = movies.find(
                     (favMovie) => favMovie._id === movieId
                   );
-                  return (
-                    <Card style={{ width: '18rem'}} key={movieId}>
-                      <Card.Img variant="top"/>
-                      <Card.Body>
-                        <Card.Title className="movie-card-title">
-                          {movie.Title}
-                        </Card.Title>
-                      </Card.Body>
-                          <Button
-                            variant="secondary"
-                            size="sm"
-                            value={movie._id}
-                            onClick={(e) => this.removeFavoriteMovie(e, movie)}
-                          >
-                            Remove
-                          </Button>
-                    </Card>
-                  );
+                  if (movie) {
+                    return (
+                      <Card style={{ width: "18rem" }} key={movieId}>
+                        <Card.Img variant="top" />
+                        <Card.Body>
+                          <Card.Title className="movie-card-title">
+                            {movie.Title}
+                          </Card.Title>
+                        </Card.Body>
+                        <Button
+                          variant="secondary"
+                          size="sm"
+                          value={movie._id}
+                          onClick={(e) => this.removeFavoriteMovie(e, movie)}
+                        >
+                          Remove
+                        </Button>
+                      </Card>
+                    );
+                  } else {
+                    return "The list is empty."
+                  }
                 })}
             </Card>
-            </React.Fragment>
+          </React.Fragment>
         </Card>
       </Row>
     );
